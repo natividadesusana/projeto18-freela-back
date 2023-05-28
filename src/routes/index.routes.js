0e0544e54706fa71bebc657e7f33430a017c7182
natividadesusana <susanajdsn@gmail.com>
2023-05-28 03:00:13 -0300

    fix: adjustments in routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import citiesRouter from "./cities.routes.js";
 import flightsRouter from "./flights.routes.js";
 import hotelsRouter from "./hotels.routes.js";
-import ticketsRouter from "./tickets.routes.js";
 import conveniencesRouter from "./conveniences.routes.js";
 
 const router = Router();
@@ -11,6 +10,5 @@ router.use(citiesRouter);
 router.use(conveniencesRouter);
 router.use(flightsRouter);
 router.use(hotelsRouter);
-router.use(ticketsRouter);
 
 export default router;
